6fbe021b02468ece5c4b3e778c0b54228143ce87
Maruf Hossain <maruf.bshs@gmail.com>
2026-01-02 15:17:31 +0600

    added views in configs app

--- backend/configs/views.py
+++ backend/configs/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.views import APIView # type: ignore
+from rest_framework.response import Response # type: ignore
+from rest_framework import status # type: ignore
+from rest_framework.permissions import IsAdminUser # type: ignore
+from .models import EmailConfig, SMSConfig, SSLCommerzConfig
+from .serializers import *
 
-# Create your views here.
+
+class SingletonConfigAPIView(APIView):
+    permission_classes = [IsAdminUser]
+    model = None
+    serializer_class = None
+
+    def get_object(self):
+        return self.model.get_solo()
+
+    def get(self, request):
+        obj = self.get_object()
+        serializer = self.serializer_class(obj)
+        return Response(serializer.data)
+
+    def put(self, request):
+        obj = self.get_object()
+        serializer = self.serializer_class(obj, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def patch(self, request):
+        obj = self.get_object()
+        serializer = self.serializer_class(
+            obj, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+
+class EmailConfigAPIView(SingletonConfigAPIView):
+    model = EmailConfig
+    serializer_class = EmailConfigSerializer
+
+
+class SMSConfigAPIView(SingletonConfigAPIView):
+    model = SMSConfig
+    serializer_class = SMSConfigSerializer
+
+
+class SSLCommerzConfigAPIView(SingletonConfigAPIView):
+    model = SSLCommerzConfig
+    serializer_class = SSLCommerzConfigSerializer
+
