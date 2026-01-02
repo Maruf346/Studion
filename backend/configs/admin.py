bd516fbec95d5c9d1fc355c821c0bed529413f58
Maruf Hossain <maruf.bshs@gmail.com>
2026-01-02 15:18:59 +0600

    added admin.py in configs app

--- backend/configs/admin.py
+++ backend/configs/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import EmailConfig, SMSConfig, SSLCommerzConfig
 
-# Register your models here.
+
+class SingletonAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return not self.model.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(EmailConfig)
+class EmailConfigAdmin(SingletonAdmin):
+    list_display = ("email_host", "email_host_user")
+
+
+@admin.register(SMSConfig)
+class SMSConfigAdmin(SingletonAdmin):
+    list_display = ("provider_name", "sender_id")
+
+
+@admin.register(SSLCommerzConfig)
+class SSLCommerzConfigAdmin(SingletonAdmin):
+    list_display = ("store_id", "sandbox")
