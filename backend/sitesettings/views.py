f93d2e0e99481f29ce6a9c46b715c1cee656998c
Maruf Hossain <maruf.bshs@gmail.com>
2026-01-05 20:53:03 +0600

    added views in sitesettings app

--- backend/sitesettings/views.py
+++ backend/sitesettings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets # type: ignore
+from .models import *
+from .serializers import *
+from user.permissions import IsAdminOrReadOnly
 
 # Create your views here.
+class FooterViewSet(viewsets.ModelViewSet):
+    queryset = Footer.objects.all()
+    serializer_class = FooterSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class SupportViewSet(viewsets.ModelViewSet):
+    queryset = support.objects.all()
+    serializer_class = SupportSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
