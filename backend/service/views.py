ccf6790bd39543491f5de6c93c1d59061a10950f
Maruf Hossain <maruf.bshs@gmail.com>
2026-01-05 20:42:38 +0600

    added views in service app

--- backend/service/views.py
+++ backend/service/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import ServiceCategory, Service
+from .serializers import *
+from user.permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class ServiceCategoryViewSet(ModelViewSet):
+    queryset = ServiceCategory.objects.prefetch_related("services")
+    serializer_class = ServiceCategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class ServiceViewSet(ModelViewSet):
+    queryset = Service.objects.select_related("category")
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
