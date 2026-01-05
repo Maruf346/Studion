80371ed74a4a0ec070f9d6b05d0aeb8aa146b53a
Maruf Hossain <maruf.bshs@gmail.com>
2026-01-05 20:43:04 +0600

    modified admin.py

--- backend/service/admin.py
+++ backend/service/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ServiceInline(admin.TabularInline):
+    model = Service
+    extra = 1
+
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    inlines = [ServiceInline]
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
